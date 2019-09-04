6f38082f28076ea4208ca8675af98669975f103b
Alex Rutar <alexrutarhome@gmail.com>
2019-09-03 22:54:55 -0400

    update TransitionMatrix to use generic probabilities

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
-from finite_type.algorithms import run_ifs
-from finite_type.examples import *
+from ifstype.examples import *
+from ifstype import run_ifs, verify_wft
+
+
 if __name__ == "__main__":
-    ifs = wft_3()
-    run_ifs(ifs,"output")
+    ifs = wft_4()
+    tr_g = run_ifs(ifs, "output",with_gens=True,scale='wide',edge_labels='index')
+
+    #  from ifstype.exact.symbolic import *
+
+
+
+    #  syr = SymbolicRing(("p1","p2","p3"))
+    #  p1,p2,p3 = syr.get_symbols()
+    #  n = p1+p2+p3**2
+    #  syr.set_eval({"p1":Rational(1,2),"p2":Rational(1,10),"p3":Rational(3,4)})
+
+    #  print(0 == p1**2+2*p1*p2+p2**2- (p1+p2)**2)
 
 
